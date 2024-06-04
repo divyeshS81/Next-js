0626329d9e09d0e0cb4f984c5cfe503e98d87fed
divyeshS81 <divyeshsarvaiya999@gmail.com>
2024-06-04 12:30:33 +0530

    add hero section is done

--- music-project/src/app/page.tsx
+++ music-project/src/app/page.tsx
@@ -1,9 +1,10 @@
+import HeroSection from "@/components/HeroSection";
 
 
 export default function Home() {
   return (
      <main className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.02]">
-       <h1 className="text-2xl text-center">Chai aur code</h1>
+       <HeroSection />
      </main>
   );
 }
